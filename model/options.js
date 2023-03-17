4f9ab30fba1b7063ebf90f7d60d91ec55f02ab57
Eva Lateltin <evalateltin@gmail.com>
2023-03-17 19:26:25 +0100

    DOCS: Add jsdoc comments

--- model/options.js
+++ model/options.js
@@ -1,23 +1,25 @@
+// All the general options of the game can be changed inside this object
+
 const options = {
-    ticketQuantity: {
+    ticketQuantity: { // Minimun and maximum number of tickets that can be generated
         min: 1,
         max: 5
     },
     ticketFeatues: {
         cities: ['Bari', 'Cagliari', 'Firenze', 'Genova', 'Milano', 'Napoli', 'Palermo', 'Roma', 'Torino', 'Venezia'],
         type: ['Ambata', 'Ambo', 'Terno', 'Quaterna', 'Cinquina'],
-        typeMinNumber: {
-            'Ambata': 2,
+        typeMinNumber: { // Based on the ticket type, the minimum quantity of numbers that will be possible to generate
+            'Ambata': 1,
             'Ambo': 2,
             'Terno': 3,
             'Quaterna': 4,
             'Cinquina': 5
         },
-        numberQuantity: {
+        numberQuantity: { // How many numbers will have each ticket
             min: 1,
             max: 10
         },
-        randomNumber: {
+        randomNumber: { // Range of the numbers that will be generated
             min: 1,
             max: 90
         }
