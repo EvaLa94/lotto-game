8bea68b102f9d3feeced1f138e9da2ec4b8e759a
Eva Lateltin <evalateltin@gmail.com>
2023-04-27 20:25:00 +0200

    REFACTOR validation tests

--- test/validation.test.js
+++ test/validation.test.js
@@ -38,20 +38,14 @@ describe("Validation.js", () => {
   });
 
   describe("checkCity", () => {
-    test("Should return true if the list of the cities includes 'Tutte'.", () => {
-      const actual = validation.checkCity(["Tutte"]);
+    test("Should return true if the city is equal to 'Tutte'.", () => {
+      const actual = validation.checkCity("Tutte");
       const expected = true;
       expect(actual).toEqual(expected);
     });
 
     test("Should not return true if the entered city is not included in the Lotto cities.", () => {
-      const actual = validation.checkCity(["London"]);
-      const notExpected = true;
-      expect(actual).not.toEqual(notExpected);
-    });
-
-    test("Should not return true if at least of the entered cities is not included in the Lotto cities.", () => {
-      const actual = validation.checkCity(["London", "Milano", "Roma"]);
+      const actual = validation.checkCity("London");
       const notExpected = true;
       expect(actual).not.toEqual(notExpected);
     });
@@ -86,63 +80,97 @@ describe("Validation.js", () => {
     });
   });
 
+  describe("checkBet", () => {
+    test("Should return true if the entered bet is a number.", () => {
+      const actual = validation.checkBet(1);
+      const expected = true;
+      expect(actual).toEqual(expected);
+    });
+
+    test("Should return a message error if the entered id is not a number.", () => {
+      const actual = validation.checkBet(["Hello", "World"]);
+      const expected = "The bet should be a number";
+      expect(actual).toEqual(expected);
+    });
+  });
+
   describe("validateEnteredFeatures", () => {
     test("Should return true if all the entered features are validated.", () => {
       const features = {
+        id: 1,
         type: "Ambo",
-        city: ["Bari", "Cagliari", "Firenze"],
+        city: "Firenze",
         quantity: 3,
+        bet: 1.5,
       };
 
-      const actual = validation.validateEnteredFeatures(1, features);
+      const actual = validation.validateEnteredFeatures(
+        ...Object.values(features)
+      );
       const expected = true;
       expect(actual).toEqual(expected);
     });
 
     test("Should not return true if the id is not validated.", () => {
       const features = {
+        id: null,
         type: "Ambo",
-        city: ["Bari", "Cagliari", "Firenze"],
+        city: "Firenze",
         quantity: 3,
+        bet: 1.5,
       };
 
-      const actual = validation.validateEnteredFeatures(null, features);
+      const actual = validation.validateEnteredFeatures(
+        ...Object.values(features)
+      );
       const notExpected = true;
       expect(actual).not.toEqual(notExpected);
     });
 
     test("Should not return true if the type is not validated.", () => {
       const features = {
+        id: 1,
         type: "Decina",
-        city: ["Bari", "Cagliari", "Firenze"],
+        city: "Firenze",
         quantity: 3,
+        bet: 1.5,
       };
 
-      const actual = validation.validateEnteredFeatures(1, features);
+      const actual = validation.validateEnteredFeatures(
+        ...Object.values(features)
+      );
       const notExpected = true;
       expect(actual).not.toEqual(notExpected);
     });
 
     test("Should not return true if the city is not validated.", () => {
       const features = {
+        id: 1,
         type: "Ambo",
-        city: ["London", "Cagliari", "Firenze"],
+        city: "London",
         quantity: 3,
+        bet: 1.5,
       };
 
-      const actual = validation.validateEnteredFeatures(1, features);
+      const actual = validation.validateEnteredFeatures(
+        ...Object.values(features)
+      );
       const notExpected = true;
       expect(actual).not.toEqual(notExpected);
     });
 
     test("Should not return true if the quantity is not validated.", () => {
       const features = {
+        id: 1,
         type: "Ambo",
-        city: ["Bari", "Cagliari", "Firenze"],
+        city: "Firenze",
         quantity: -3,
+        bet: 1.5,
       };
 
-      const actual = validation.validateEnteredFeatures(1, features);
+      const actual = validation.validateEnteredFeatures(
+        ...Object.values(features)
+      );
       const notExpected = true;
       expect(actual).not.toEqual(notExpected);
     });
@@ -151,11 +179,13 @@ describe("Validation.js", () => {
   describe("generateTicket", () => {
     test("Should generate a ticket if the features are validated.", () => {
       const features = {
+        id: 1,
         type: "Ambo",
-        city: ["Bari", "Cagliari", "Firenze"],
+        city: "Firenze",
         quantity: 3,
+        bet: 1.5,
       };
-      const ticket = validation.generateTicket(1, features);
+      const ticket = validation.generateTicket(...Object.values(features));
       const actual = ticket instanceof Ticket;
       const expected = true;
       expect(actual).toEqual(expected);
@@ -163,11 +193,13 @@ describe("Validation.js", () => {
 
     test("Should not generate a ticket if at least one feature is not validated.", () => {
       const features = {
+        id: 1,
         type: "Decina",
-        city: ["Bari", "Cagliari", "Firenze"],
+        city: "Firenze",
         quantity: 3,
+        bet: 1.5,
       };
-      const ticket = validation.generateTicket(1, features);
+      const ticket = validation.generateTicket(...Object.values(features));
       const actual = ticket instanceof Ticket;
       const expected = false;
       expect(actual).toEqual(expected);
