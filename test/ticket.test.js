77566fc7ae21615c9ae8c24c0dfd7dd246326bc1
Eva Lateltin <evalateltin@gmail.com>
2023-05-07 11:15:30 +0200

    TEST add ticket test

--- test/ticket.test.js
+++ test/ticket.test.js
@@ -1,4 +1,5 @@
 const Ticket = require("../model/ticket.js");
+const Extraction = require("../model/extraction.js");
 const { optionsTicket } = require("../controller/optionGame/optionsTicket.js");
 
 describe("Ticket", () => {
@@ -41,6 +42,24 @@ describe("Ticket", () => {
       const expected = features.bet;
       expect(actual).toEqual(expected);
     });
+
+    test("The new ticket should have a isWinning property set to false.", () => {
+      const actual = ticket.isWinning;
+      const expected = false;
+      expect(actual).toEqual(expected);
+    });
+
+    test("The new ticket should have a grossWin property set to 0.", () => {
+      const actual = ticket.grossWin;
+      const expected = 0;
+      expect(actual).toEqual(expected);
+    });
+
+    test("The new ticket should have a netWin property set to 0.", () => {
+      const actual = ticket.netWin;
+      const expected = 0;
+      expect(actual).toEqual(expected);
+    });
   });
 
   describe("Particular case.", () => {
@@ -76,6 +95,44 @@ describe("Ticket", () => {
       ticket.numbers = [23, 45, 86];
       const actual = ticket.printTicket();
       const expected = `+------------------------+\n|       TICKET #1        |\n+---------+--------------+\n| City    | Bari         |\n| Type    | Ambo         |\n| Numbers | 23 - 45 - 86 |\n| Bet     | 1.50 €       |\n+---------+--------------+\n`;
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe("checkWinningTicket", () => {
+    const extraction = new Extraction();
+    const ticket = new Ticket(1, "Cinquina", "Roma", 5, 1);
+    extraction.Roma = [1, 2, 3, 4, 5];
+    ticket.numbers = [1, 2, 3, 4, 5];
+    ticket.checkWinningTicket(extraction);
+
+    test("A winning ticket should have a isWinning property set to true", () => {
+      const actual = ticket.isWinning;
+      const expected = true;
+      expect(actual).toEqual(expected);
+    });
+
+    test("A winning ticket should have a grossWin property greater than 0", () => {
+      const actual = ticket.grossWin;
+      const expected = 0;
+      expect(actual).toBeGreaterThan(expected);
+    });
+
+    test("A winning ticket should have a netWin property greater than 0", () => {
+      const actual = ticket.netWin;
+      const expected = 0;
+      expect(actual).toBeGreaterThan(expected);
+    });
+
+    test("A winning ticket with 5 numbers of type 'Cinquina' should win a gross amount of 6.000.000€", () => {
+      const actual = ticket.grossWin;
+      const expected = 6000000;
+      expect(actual).toEqual(expected);
+    });
+
+    test("A winning ticket with 5 numbers of type 'Cinquina' should win a net amount of 6.000.000€", () => {
+      const actual = ticket.netWin;
+      const expected = 5520000;
       expect(actual).toEqual(expected);
     });
   });
